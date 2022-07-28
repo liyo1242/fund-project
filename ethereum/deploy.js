b85e3841f8f08c51c38755496e9b73fa3d074749
liyo <liyo1242@gmail.com>
2022-07-28 23:03:28 +0800

    build: add deploy file

---
+++ ethereum/deploy.js
@@ -0,0 +1,22 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const Web3 = require('web3')
+const compiledFactory = require('../ethereum/build/FundraisingFactory.json')
+
+const provider = new HDWalletProvider(
+  env.process.PNEUMONIC,
+  env.process.RINKEBY_ADDRESS
+)
+
+const web3 = new Web3(provider)
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts()
+
+  const result = await new web3.eth.Contract(compiledFactory.abi)
+    .deploy({ data: compiledFactory.evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] })
+
+  console.log('success deploy contract to rinkby', result.options.address)
+}
+
+deploy()
