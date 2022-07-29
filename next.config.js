2ca552274d46631b133d8a9ecff2f2c3da4732d2
liyo <liyo1242@gmail.com>
2022-07-29 17:58:31 +0800

    feat: add develop support

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
